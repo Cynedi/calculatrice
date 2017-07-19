a05b50b8d84bdf505bde89c41a356751ab51e6c7
cindy <cindy.nicolay@laposte.net>
2017-07-19 14:55:49 +0200

    function onclik et affichage des operateurs

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 //Mes variables//
 
-var leInput=document.getElementsByTagName('th')[0];
-var leNumeral=document.getElementsByClassName('numeral');
-var operator=document.getElementsByClassName('operator');
+var ecran=document.getElementsByTagName('th')[0];
+var numbers=document.getElementsByClassName('numeral');
+var operande=document.getElementsByClassName('operator');
 var result=document.getElementById('result');
 var clear=document.getElementById('reset');
 
-//On va mettre une fonction clique sur chaque chiffre de la calculatrice//
-for (let i = 0; i < leNumeral.length; i++) {
-  // console.log(leNumeral);
-  leNumeral[i].addEventListener("click", function() {
-     var affichage = leNumeral[i].textContent;
-    // alert(affichage);
-    leInput.innerHTML = affichage;
-    // var x = affichage[affichage.length - 1];
 
-    // console.log (affichage);
+for (let i = 0; i < numbers.length; i++) {
+
+//On va mettre une fonction clique sur chaque chiffre de la calculatrice et l'afficher//
+  numbers[i].addEventListener("click", function() {
+     var affichage = numbers[i].textContent;
+     ecran.innerHTML += affichage;
+
+});
+}
+
+for (let j = 0; j < operande.length; j++) {
+
+//On va mettre une fonction clique sur chaque operateur de la calculatrice et l'afficher//
+  operande[j].addEventListener("click", function() {
+     var affich = operande[j].textContent;
+     ecran.innerHTML += affich;
+
 });
 }
