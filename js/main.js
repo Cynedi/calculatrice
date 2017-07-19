d9a5ae23eec5c4cc427ce7e251091a7878725b9f
cindy <cindy.nicolay@laposte.net>
2017-07-19 13:43:06 +0200

    function onclik et affichage des chiffres à l'ecran

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 //Mes variables//
 
-var input=document.getElementsByTagName('input');
-var numeral=document.getElementsByClassName('numeral');
+var leInput=document.getElementsByTagName('th')[0];
+var leNumeral=document.getElementsByClassName('numeral');
 var operator=document.getElementsByClassName('operator');
 var result=document.getElementById('result');
 var clear=document.getElementById('reset');
+
+//On va mettre une fonction clique sur chaque chiffre de la calculatrice//
+for (let i = 0; i < leNumeral.length; i++) {
+  // console.log(leNumeral);
+  leNumeral[i].addEventListener("click", function() {
+     var affichage = leNumeral[i].textContent;
+    // alert(affichage);
+    leInput.innerHTML = affichage;
+    // var x = affichage[affichage.length - 1];
+
+    // console.log (affichage);
+});
+}
