9ceb09f06d9c83cf62edc428e0a6e3240a1803a8
cindy <cindy.nicolay@laposte.net>
2017-07-19 10:33:20 +0200

    made variables

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Mes variables//
+
+var input=document.getElementsByTagName('input');
+var numeral=document.getElementsByClassName('numeral');
+var operator=document.getElementsByClassName('operator');
+var result=document.getElementById('result');
+var clear=document.getElementById('reset');
