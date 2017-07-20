7f50f14f1cbf1231c697dab9537374680590cece
cindy <cindy.nicolay@laposte.net>
2017-07-20 11:17:03 +0200

    function result avec eval

--- js/main.js
+++ js/main.js
@@ -4,16 +4,17 @@ var ecran=document.getElementsByTagName('th')[0];
 var numbers=document.getElementsByClassName('numeral');
 var operande=document.getElementsByClassName('operator');
 var clear=document.getElementById('reset');
+var res=document.getElementById('resultat');
+var display=document.getElementById('display');
 
-
-
+console.log(ecran.value);
 //On crée une boucle qui parcoure le tableau des chiffres//
 for (let i = 0; i < numbers.length; i++) {
 
 //On va mettre une fonction clique sur chaque chiffre de la calculatrice et l'afficher//
   numbers[i].addEventListener("click", function() {
-     var affichage = numbers[i].textContent;//recupère le contenu de chaque bouton chiffre//
-     ecran.innerHTML += affichage;//nouveau contenu pour l'ecran//
+     var affichage = numbers[i].value;//recupère le contenu de chaque bouton chiffre//
+     display.value +=affichage ;//nouveau contenu pour l'ecran//
 
 });
 }
@@ -23,13 +24,14 @@ for (let j = 0; j < operande.length; j++) {
 
 //On va mettre une fonction clique sur chaque operateur de la calculatrice et l'afficher//
   operande[j].addEventListener("click", function() {
-     var affich = operande[j].textContent;//recupère le contenu de chaque bouton operateur//
-     ecran.innerHTML += affich;//nouveau contenu pour l'ecran//
+     var affich = operande[j].value;//recupère le contenu de chaque bouton operateur//
+     display.value +=affich;//nouveau contenu pour l'ecran//
 });
 }
 
-function resultat() {//calcul le contenu actuel//
-   var x = 0; //variable ou le resultat est sauvegardé//
-   x = eval(window.document.calculatrice.affiche.value);//l'opération de calcul transmise à eval qui calcul automatiquement//
-   window.document.calculatrice.affiche.value = x; //affecté au champs de formulaire pour etre affiché//
+function result() {//calcul le contenu actuel//
+ //variable ou le resultat est sauvegardé//
+   display.value = eval (display.value);//l'opération de calcul transmise à eval qui calcul automatiquement//
+   //affecté au champs de formulaire pour etre affiché//
+
  }
